a2b85a018e4c4eebd56999c371c0a1a94fafb37b
Gabjea <43420432+Gabjea@users.noreply.github.com>
2022-03-16 21:12:10 +0200

    fixed compiler + optimization

---
+++ backend/uploads/compile/pow/6228fdca8f7638b1d65949b8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    //add your code here
+  	int a, b;
+  	cin >> a >> b;
+  	cout << pow(a, b) << endl;
+    return 0;
+}
+    
