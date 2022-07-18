3f8c48e73744261d2055d41ee60ee51d43b80f6c
Gabjea <43420432+Gabjea@users.noreply.github.com>
2022-07-18 11:09:04 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const server = require("./app");
 
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
 });
