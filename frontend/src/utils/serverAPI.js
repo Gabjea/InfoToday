27e25f45f3c4666ba8ca89a64ca84e47846c3fda
Gabjea <43420432+Gabjea@users.noreply.github.com>
2022-07-18 12:52:22 +0300

    Update serverAPI.js

--- frontend/src/utils/serverAPI.js
+++ frontend/src/utils/serverAPI.js
@@ -7,7 +7,7 @@ export const host = 'backend-infotoday.herokuapp.com';
 export const hostWithPort = `${host}:5000`;
 export const baseHttpURL = `https://${host}`;
 export const baseURLPref = `${baseHttpURL}/api/v1`;
-export const baseWsURL = `ws://${baseHttpURL}`;
+export const baseWsURL = `wss://${host}`;
 const timeout = 30000e3;
 
 export const axiosInstanceToAPI = axios.create({
