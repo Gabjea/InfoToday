bbb0d20d48da4c9463fc2c0ece76b82143ebff36
Gabjea <43420432+Gabjea@users.noreply.github.com>
2022-07-18 16:26:25 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,8 +27,11 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   next();
 });
 const functions = require('./api/functions')
