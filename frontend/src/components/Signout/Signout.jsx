fb54b46bd3f30f90bb02b96f04e1a1de5183ae22
Gabjea <43420432+Gabjea@users.noreply.github.com>
2022-07-26 13:38:30 +0300

    Update Signout.jsx

--- frontend/src/components/Signout/Signout.jsx
+++ frontend/src/components/Signout/Signout.jsx
@@ -6,12 +6,14 @@ export default function Signout()  {
     
     React.useEffect(() => {
         CookieManager.deleteCookie('jwt');
+        window.location.reload();
+    })
+    React.useEffect(() => {
         if (CookieManager.getCookie('jwt') == null) {
             window.location.assign('/login');
             return;
         }
-        
-    })
+    }, [])
 
     return (
         <div>
